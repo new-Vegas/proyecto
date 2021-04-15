02fe364c06c4b4f3f4073f841979edaeee9e90ed
David Moncivais <davidmoncivais@live.com.mx>
2021-04-15 12:00:43 -0700

    Update prototipo.sql

--- información/prototipo.sql
+++ información/prototipo.sql
@@ -3,6 +3,7 @@ INSERT INTO categories (ES_name, EN_name, slug) VALUES ('Colaboradores', 'Collab
 INSERT INTO categories (ES_name, EN_name, slug) VALUES ('Productos', 'Products', 'product');
 INSERT INTO categories (ES_name, EN_name, slug) VALUES ('Articulos', 'Articles', 'articles');
 INSERT INTO categories (ES_name, EN_name, slug) VALUES ('Noticias', 'News', 'news');
-INSERT INTO categories (ES_name, EN_name, slug) VALUES ('Foros', 'forums', 'Foros');
+INSERT INTO categories (ES_name, EN_name, slug) VALUES ('Foros', 'Forums', 'Foros');
+INSERT INTO categories (ES_name, EN_name, slug) VALUES ('Contactanos', 'Contact us', 'contact');
 
 INSERT INTO user_types (name, nameES, descENG, descES) VALUES (' ', ' ', ' ', ' ');
