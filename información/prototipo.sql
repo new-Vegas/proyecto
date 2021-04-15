fbef7b5da79a802b7d30def787bee0c69a7243cd
David Moncivais <davidmoncivais@live.com.mx>
2021-04-15 10:42:32 -0700

    base de la pagina
    -se crea carpeta de para llenar la base de datos.
    -se cambian los datos de la pagina principal.

---
+++ información/prototipo.sql
@@ -0,0 +1,8 @@
+INSERT INTO categories (ES_name, EN_name, slug) VALUES ('Participantes', 'Participants', 'participants');
+INSERT INTO categories (ES_name, EN_name, slug) VALUES ('Colaboradores', 'Collaborators', 'collab');
+INSERT INTO categories (ES_name, EN_name, slug) VALUES ('Productos', 'Products', 'product');
+INSERT INTO categories (ES_name, EN_name, slug) VALUES ('Articulos', 'Articles', 'articles');
+INSERT INTO categories (ES_name, EN_name, slug) VALUES ('Noticias', 'News', 'news');
+INSERT INTO categories (ES_name, EN_name, slug) VALUES ('Foros', 'forums', 'Foros');
+
+INSERT INTO user_types (name, nameES, descENG, descES) VALUES (' ', ' ', ' ', ' ');
