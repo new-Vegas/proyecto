eb1b37dbfdd624911af82e4a49e7fb3c5dfc67a2
wazazky9 <78938825+wazazky9@users.noreply.github.com>
2021-04-19 11:24:49 -0700

    Add files via upload
    Okay , creo que lo puse en los comentarios  pero a la hora de hacer la asignación a la pestaña indicada revisar el id creado

---
+++ información/Colabs.sql
@@ -0,0 +1,22 @@
+INSERT INTO posts (user_id, usr_type_id, name, name_ES, slug, image, content, content_ES, enabled)  /*se genera el post*/
+VALUES (1, 1, 'Hospital Infantil de las Californias', 'Hospital Infantil de las Californias', 'Hospital', 'https://doprod-statics.s3.amazonaws.com/pictures/logo/6409/large_c6d3ac69-7dae-4b62-8e15-0b53f71d19f4.png ', ' Hospital Infantil de las Californias ,https://hospitalinfantil.org/ ', 'Hospital Infantil de las Californias ,https://hospitalinfantil.org/', 1);
+INSERT INTO posts (user_id, usr_type_id, name, name_ES, slug, image, content, content_ES, enabled)  /*se genera el post*/
+VALUES (1, 1, 'Karla Prado', 'Karla Prado', 'Karla-Prado', 'https://www.cimec.co.za/wp-content/uploads/2018/07/4-Unique-Placeholder-Image-Services-for-Designers.png ', 'Karla Prado nutkepp@gmail.com', 'Karla Prado nutkepp@gmail.com ', 1);
+INSERT INTO posts (user_id, usr_type_id, name, name_ES, slug, image, content, content_ES, enabled)  /*se genera el post*/
+VALUES (1, 1, 'Marcela García Castillo', 'Marcela García Castillo', 'Marcela-Garcia-Castillo', 'https://www.cimec.co.za/wp-content/uploads/2018/07/4-Unique-Placeholder-Image-Services-for-Designers.png ', 'Marcela García Castillo nut.marcela.garcia@gmail.com', 'Marcela García Castillo nut.marcela.garcia@gmail.com', 1);
+INSERT INTO posts (user_id, usr_type_id, name, name_ES, slug, image, content, content_ES, enabled)  /*se genera el post*/
+VALUES (1, 1, 'Universidad de Puerto Rico, Recinto de Río Piedras Escuela de Comunicación', 'Universidad de Puerto Rico, Recinto de Río Piedras Escuela de Comunicación', 'UPRRRPEC', 'https://www.cimec.co.za/wp-content/uploads/2018/07/4-Unique-Placeholder-Image-Services-for-Designers.png', 'Universidad de Puerto Rico, Recinto de Río Piedras Escuela de Comunicación, http://copu.uprrp.edu/ ', 'Universidad de Puerto Rico, Recinto de Río Piedras Escuela de Comunicación, http://copu.uprrp.edu/ ', 1);
+INSERT INTO posts (user_id, usr_type_id, name, name_ES, slug, image, content, content_ES, enabled)  /*se genera el post*/
+VALUES (1, 1, 'Dra. Wanda Reyes Velázquez', 'Dra. Wanda Reyes Velázquez', 'Dra-Wanda-Reyes-Velazquez', 'https://www.cimec.co.za/wp-content/uploads/2018/07/4-Unique-Placeholder-Image-Services-for-Designers.png ', 'Dra. Wanda Reyes Velázquez, wanda.reyes@upr.edu', 'Dra. Wanda Reyes Velázquez, wanda.reyes@upr.edu', 1);
+INSERT INTO posts (user_id, usr_type_id, name, name_ES, slug, image, content, content_ES, enabled)  /*se genera el post*/
+VALUES (1, 1, 'Tanya P De Jesús-Rodríguez', 'Tanya P De Jesús-Rodríguez', 'Tanya-P-De-Jesus-Rodriguez,', 'https://www.cimec.co.za/wp-content/uploads/2018/07/4-Unique-Placeholder-Image-Services-for-Designers.png ', 'Tanya P De Jesús-Rodríguez, tanya.dejesus@upr.edu ', 'Tanya P De Jesús-Rodríguez, tanya.dejesus@upr.edu ', 1);
+INSERT INTO posts (user_id, usr_type_id, name, name_ES, slug, image, content, content_ES, enabled)  /*se genera el post*/
+VALUES (1, 1, 'ALAIC COORDINACIÓN GT Comunicación y Salud', 'ALAIC COORDINACIÓN GT Comunicación y Salud','ACGTCS', 'https://www.cimec.co.za/wp-content/uploads/2018/07/4-Unique-Placeholder-Image-Services-for-Designers.png ', 'ALAIC COORDINACIÓN GT Comunicación y Salud, https://www.alaic.org/ alaic.gtcys.coordinacion@gmail.com', 'ALAIC COORDINACIÓN GT Comunicación y Salud, https://www.alaic.org/ alaic.gtcys.coordinacion@gmail.com', 1);
+
+INSERT INTO category_posts (post_id, category_id) VALUES (2, 2);/*se anexa el post a una categoria , revisa ccon cual Id se genero el post de la liena anterior */
+INSERT INTO category_posts (post_id, category_id) VALUES (3, 2);
+INSERT INTO category_posts (post_id, category_id) VALUES (4, 2);
+INSERT INTO category_posts (post_id, category_id) VALUES (5, 2);
+INSERT INTO category_posts (post_id, category_id) VALUES (6, 2);
+INSERT INTO category_posts (post_id, category_id) VALUES (7, 2);
+INSERT INTO category_posts (post_id, category_id) VALUES (8, 2);
