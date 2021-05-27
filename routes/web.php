dcf74e12070eee3d791ad261d2e86345b3249174
wazazky9 <78938825+wazazky9@users.noreply.github.com>
2021-05-26 19:56:51 -0700

    direccion de sugerencias
    creo que sobra una linea de search , no se si se pueda borrar

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::get('/post/{post:slug}', [PageController::class, 'post'])->name('post');
 Route::get('/survey/{survey:slug}', [PageController::class, 'survey'])->name('survey');
 Route::get('/search/', [PageController::class, 'search'])->name('search');
 Route::get('/search/', [PageController::class, 'search'])->name('search');
+Route::get('/sugerencias/', [PageController::class, 'sugerencias'])->name('sugerencias'); 
 Route::get('/Category/{category:slug}', [PageController::class, 'category'])->name('category');
 // Admin panel
 
