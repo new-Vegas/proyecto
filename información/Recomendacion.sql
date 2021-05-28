543f2eaa45ea0f02212f37947dc628522dea2a68
wazazky9 <78938825+wazazky9@users.noreply.github.com>
2021-05-27 18:57:48 -0700

    Add files via upload

---
+++ información/Recomendacion.sql
@@ -0,0 +1,2 @@
+INSERT INTO `categories` (`id`, `ES_name`, `EN_name`, `slug`, `created_at`, `updated_at`) VALUES (NULL, 'recomendacion', 'recommendation', 'recommendation', NULL, NULL)
+INSERT INTO `category_posts` (`id`, `post_id`, `category_id`, `created_at`, `updated_at`) VALUES (NULL, '5', '7', NULL, NULL)
