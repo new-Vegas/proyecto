fa461ab321f77d6708b1a0690f64dcedabfe1fa0
benjamin-cota <benjamin.cota@uabc.edu.mx>
2021-06-09 02:27:35 -0700

    Subscripcion terminada con traduccion

--- resources/js/Pages/subscription.jsx
+++ resources/js/Pages/subscription.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Layout from "../Components/Layout";
-
+import { useTranslation } from 'react-i18next';
 
 const Home = () => {
-    
+    const { t, i18n } = useTranslation();
 
     return (
 
         <div className="main-content">
             <div id="mc_embed_signup">
-                <form action="https://uabc.us6.list-manage.com/subscribe/post?u=030b610020d1ae1879c1643e9&amp;id=6b65691459" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form"  target="_blank" novalidate>
+                <form action="https://gmail.us6.list-manage.com/subscribe/post?u=f157bf87f003ebfa8b4028541&amp;id=76d03328a4" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form"  target="_blank" novalidate>
                     <div id="mc_embed_signup_scroll">
-	                    <h2>Subscribe</h2>
+	                    <h2>{t('Subscribe')}</h2>
+                        <p>{t('writeEmail')}</p>
                         <div >
 	                        <input type="email"  name="EMAIL"  id="mce-EMAIL"></input>
                         </div>
